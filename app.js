7f51de8f02ac8dad6378e57d62a1084146fcb871
Eml-coder <labiche6789@gmail.com>
2022-12-12 21:12:27 -0500

    implement the feature of adding new items to the list

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
+const quoteWrapper = document.getElementById('quote-wrapper');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const tweetBttn = document.getElementById('tweet');
+const newQuoteBttn = document.getElementById('new-quote');
+
 //Setting up API
-let api = [];
-const pullQuotes = async () => {
+let apiQ = [];
 
-} 
+const newQ = () => {
+  //get random quote
+  const randomQuote = apiQ[Math.floor(Math.random() * apiQ.length)];
+  //check if author is blank or null, if it is, we want to display 'unknown' instead of blank, if it is not blank, we want to display the author name.
+  !randomQuote.author
+    ? (authorText.textContent = 'Uknown author')
+    : (authorText.textContent = randomQuote.author);
 
-apiUrl = 'https://type.fit/api/quotes'
+  //if the quote text length is greater than 120, we want to change the font size to 30px, if it is not, we want to change the font size to 50px:
+  randomQuote.text.length > 150
+    ? quoteText.classList.add('long-quote')
+    : quoteText.classList.remove('long-quote');
+};
+
+const getQ = async () => {
+  const apiLink = 'https://type.fit/api/quotes';
+  try {
+    const reply = await fetch(apiLink);
+    apiQ = await reply.json();
+    newQ();
+  } catch (error) {
+    //will catch errors here
+  }
+};
+
+getQ();
+console.log(getQ());
