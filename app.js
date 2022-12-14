94df33b26c86229325af89d294d25dd1f7be6f1c
Eml-coder <labiche6789@gmail.com>
2022-12-13 19:05:03 -0500

    add js logic to display quote and author

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const tweetBttn = document.getElementById('tweet');
 const newQuoteBttn = document.getElementById('new-quote');
+//loaders
+// function load() {
+//   quoteWrapper.hidden = true;
+//   //loader.hidden = false;
+// }
+// function finalised () {
+//   quoteWrapper.hidden = false;
+//   //loader.hidden = true;
+// }
 
 //Setting up API
 let apiQ = [];
 
 const newQ = () => {
+  //load();
+
   //get random quote
   const randomQuote = apiQ[Math.floor(Math.random() * apiQ.length)];
   //check if author is blank or null, if it is, we want to display 'unknown' instead of blank, if it is not blank, we want to display the author name.
@@ -18,10 +29,16 @@ const newQ = () => {
   //if the quote text length is greater than 120, we want to change the font size to 30px, if it is not, we want to change the font size to 50px:
   randomQuote.text.length > 150
     ? quoteText.classList.add('long-quote')
-    : quoteText.classList.remove('long-quote');
+    : quoteText.classList.remove('long-quote'); //"long-quote" is a class in the css file?
+
+  //display quote to the interface, hide loader
+  quoteText.textContent = randomQuote.text;
+  //loader
+  //finalised();
 };
 
 const getQ = async () => {
+  //load();
   const apiLink = 'https://type.fit/api/quotes';
   try {
     const reply = await fetch(apiLink);
@@ -32,5 +49,18 @@ const getQ = async () => {
   }
 };
 
+//Twitter functionality
+const tweetQ = () => {
+  const tweetLink = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  //open a new window with the tweet link
+  window.open(tweetLink, '_blank');
+};
+
+//Event Listeners
+newQuoteBttn.addEventListener('click', newQ);
+tweetBttn.addEventListener('click', tweetQ);
+
+//getQ function is called when the page loads
 getQ();
-console.log(getQ());
+
+//load();
